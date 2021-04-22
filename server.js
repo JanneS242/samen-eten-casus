14c734ad8dd54ac68030480bbe5b1475b7ec0e60
Janne Sterk <jannesterk@hotmail.com>
2021-04-22 09:28:17 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const importData = require("./data.json");
+
+const port = process.env.PORT || 3000
+app.get("/", (req,res) => {
+  res.send("Hello World. YOU BETTER WORK");
+})
+
+app.get("/classes", (req, res) => {
+  res.send(importData);
+})
